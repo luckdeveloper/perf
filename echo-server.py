4b43ff252986e123d29f2744a2ab361ba9b076c1
luckdeveloper@gmail.com <luckdeveloper@gmail.com>
2019-10-23 16:55:11 +0800

    modify to python 2

--- echo-server.py
+++ echo-server.py
@@ -1,18 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
 import socket
 
 HOST = ''  
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
+def main(argv=None):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(socket.gethostname(), PORT)
     s.listen()
+
     conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    print('Connected by', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(data)
+
+if __name__ == "__main__":
+    main()
